97ab2b4e94676e28a1d0e7474d79f3407d06a855
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-03-14 18:18:37 -0700

    Contact form created and style for home improved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,15 @@ function Home() {
         <div>
             <Header />
             <main>
-                <div className="bio-container">
-                    <h1>Software developer in training</h1>
-                    <p>I am a first year student of computer science at langara college, I am passionate about software development and especially web development. If you want to know more about my background let check my resume!</p>
-                </div>
-                <div className="photo-container">
-                    <img alt="Photo of Cesar" src="\images\Photo-of-me.jpg"></img>
+                <div className='home-container'>
+                    <div className="background-image-home"></div>
+                    <div className="bio-container">
+                        <h1>Software developer in training</h1>
+                        <p>I am a first year student of computer science at langara college, I am passionate about software development and especially web development. If you want to know more about my background let check my resume!</p>
+                    </div>
+                    <div className="photo-container">
+                        <img alt="Photo of Cesar" src="\images\Photo-of-me.jpg"></img>
+                    </div>
                 </div>
             </main>
             <Footer />
