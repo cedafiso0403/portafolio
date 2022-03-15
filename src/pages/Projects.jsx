9ffee40f81b7b6f37dc137a050b5a41636aa9c29
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-03-14 18:58:37 -0700

    Added generator of project cards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,12 +2,24 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ProjectCard from '../components/ProjectCard';
 
-const DATA = {
-    name: "Kaisel Bot",
-    src: "",
-    description: "App for retrieving stats for League"
+const DATA = [
+    {
+        name: "Kaisel Bot",
+        src: "",
+        description: "App for retrieving stats for League"
+    },
+    {
+        name: "Transfullanos",
+        src: "",
+        description: "App for company"
+    },
+    {
+        name: "Expensify App",
+        src: "",
+        description: "Keep track of expenses"
+    }
 
-}
+]
 
 
 function Projects() {
@@ -15,8 +27,16 @@ function Projects() {
         <div>
             <Header />
             <main>
+                <div className="project-container">
+                    <h1>My Work</h1>
+                    <p>These are some of the projects I have been working on during my studies, hope you like them!</p>
+                </div>
                 <div className="cards-container">
-                    <ProjectCard {...DATA} />
+                    {
+                        DATA.map((element) => {
+                            return <ProjectCard key={element.name} {...element} />
+                        })
+                    }
                 </div>
             </main>
             <Footer />
