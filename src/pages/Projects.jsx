b1271e9dd2957bb2a07f15e7a1d429228cfe2991
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-03-14 18:46:07 -0700

    cards container created

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,24 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ProjectCard from '../components/ProjectCard';
+
+const DATA = {
+    name: "Kaisel Bot",
+    src: "",
+    description: "App for retrieving stats for League"
+
+}
+
 
 function Projects() {
     return (
         <div>
             <Header />
-
+            <main>
+                <div className="cards-container">
+                    <ProjectCard {...DATA} />
+                </div>
+            </main>
             <Footer />
         </div>
     );
